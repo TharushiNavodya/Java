3922fed97cac2e6dd903c32edb400c7f9a77db31
Tharushi Navodya <96821399+TharushiNavodya@users.noreply.github.com>
2021-12-29 10:52:21 +0000

    Hello world

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 public class Main
 {
-    public static void main(String [] args)
-    {
-        System.out.println("Hello world");
-         System.out.println("Hello world");
-          System.out.println("Hello world"); System.out.println("Hello world");
-    }
+	//In java is pure object oriented language 
+	//so smallest program use a class 
+	public static void main(String []args)
+	{
+		//output command is a java 
+		//System.out.print
+		//print hello world and go a new line 
+		
+		System.out.println("Hello World");
+		
+		//print hello world without go a new line 
+		System.out.print("Hello world");
+	}
 }
